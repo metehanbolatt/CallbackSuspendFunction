dc52f9416e510416f94d2523fee02826d207ca58
Metehan BOLAT <73827610+metehanie@users.noreply.github.com>
2022-02-10 23:45:41 +0300

    What returns from Disposable? and when?

--- app/src/main/java/com/metehanbolat/anycallbacksuspendfunction/MainActivity.kt
+++ app/src/main/java/com/metehanbolat/anycallbacksuspendfunction/MainActivity.kt
@@ -2,10 +2,30 @@ package com.metehanbolat.anycallbacksuspendfunction
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.metehanbolat.anycallbacksuspendfunction.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var mainViewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        mainViewModel = ViewModelProvider(this)[MainViewModel::class.java]
+
+        /*
+        mainViewModel.countries.observe(this) {
+            println(it)
+        }
+
+         */
+        println(mainViewModel.getDataFromApi())
+        println(mainViewModel.countries.value)
+
     }
 }
--- app/src/main/java/com/metehanbolat/anycallbacksuspendfunction/MainViewModel.kt
+++ app/src/main/java/com/metehanbolat/anycallbacksuspendfunction/MainViewModel.kt
@@ -1,15 +1,37 @@
 package com.metehanbolat.anycallbacksuspendfunction
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.metehanbolat.anycallbacksuspendfunction.model.CountryModel
 import com.metehanbolat.anycallbacksuspendfunction.service.CountryApiService
+import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.observers.DisposableSingleObserver
+import io.reactivex.schedulers.Schedulers
 
 class MainViewModel: ViewModel() {
 
     private val countryApiService = CountryApiService()
     private val disposable = CompositeDisposable()
+    val countries = MutableLiveData<List<CountryModel>>()
 
-    private fun getDataFromApi() {
+    fun getDataFromApi(): Boolean {
+        val a = disposable.add(
+            countryApiService.getData()
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(object : DisposableSingleObserver<List<CountryModel>>() {
+                    override fun onSuccess(value: List<CountryModel>?) {
+                        countries.value = value
+                    }
+                    override fun onError(e: Throwable?) {
+                        e?.let {
+                            println(it.localizedMessage)
+                        }
+                    }
 
+                })
+        )
+        return a
     }
 }
